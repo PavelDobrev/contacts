ab068ca2cd9106e276fb1ac0d14572e3af651f86
PavelDobrev <136815298+PavelDobrev@users.noreply.github.com>
2023-08-15 17:37:17 +0300

    Delete Contacts.Data directory

--- Contacts.Data/Entities/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Contacts.Data.Entities
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public ICollection<ApplicationUserContact> ApplicationUsersContacts { get; set; }
-    }
-}
